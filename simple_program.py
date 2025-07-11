ec1ebd5af9c09aa891d3e970466f1e47719649a5
priyanshu-singh874 <singhpriyanshu2908@gmail.com>
2025-07-11 16:48:50 +0530

    Uploaded simple_program.py

---
+++ simple_program.py
@@ -0,0 +1,11 @@
+import numbers
+
+
+
+a1=int(input("Enter the first number: "))
+a2=int(input("Enter the second number: "))
+
+print("Addition: ", a1+a2)
+print("Subtraction: ", a1-a2)
+print("Multiplication: ", a1*a2)
+print("Division: ", a1/a2)
